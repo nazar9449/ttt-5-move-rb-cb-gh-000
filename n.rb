908af1f5e61f39223918e05d4fc30f843caa3ca8
Nazar Turkmen <nazar.rudn@gmail.com>
2020-04-22 03:40:04 +0000

    Automatically backed up by Learn

--- n.rb
+++ n.rb
@@ -1,6 +1,9 @@
 if false
    puts "This will never get printed because the above
      statement evaluates to false."
+   elsif 5>2
+     puts "5 ikiden ulydyr"
+     
 else
    puts "This will get printed!"
 end
